7027e560b9f027d0b545e2344dbdd7c65ab3a918
Mikadevfr <mikadevfr@gmail.com>
2024-10-11 19:42:45 +0200

    HP

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
 import "./style.css";
+import Logo from './../../assets/Logo.jpg'
+import IconPhone from './../../assets/Phone.png'
+import IconMenu from './../../assets/menu.png'
 
 const HeaderContainer = styled.div`
     display: flex;
@@ -10,16 +13,20 @@ const HeaderContainer = styled.div`
 export default function Header() {
 
     return (
+
         <header>
-            HeaderContainer
-            <a href="#">
-                <img src="" alt="" />
-            </a>
-            <a href="#">
-            <img src="" alt="" />
-            </a>
+            <div>
+                <a href="#">
+                    <img src={Logo} alt="Logo Horizon Bien Etre" />
+                </a>
+            </div>
+            <div className='phone'>
+                <a href="#">
+                <img src={IconPhone} alt="Phone icon" />
+                </a>
+            </div>
             <nav>
-                <img src="" alt="" />
+                <img src={IconMenu} alt="Menu icon" />
             </nav>
         </header>
     );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,35 @@
-import styled from 'styled-components'
-
-const HomeContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-`
+import "./style.css";
+/*import img_illustration from './../../assets/Illustration_hp_big.jpg'
+import img_illustration from './../../assets/Illustration_hp_big.png'*/
 
 export default function Home() {
 
     return (
-        <HomeContainer>
-            HomeContainer
-        </HomeContainer>
+        <main>
+            
+            <div className="illustration">
+                <div>
+                    <h1>Salon Bien-Ètre</h1>
+                </div>
+            </div>
+           
+            <div className="description">
+                <h2>Horizon bien-être, un lieu de détente et de bienveillance</h2>
+                <div className="img_salon"></div>
+                <p>Mon objectif est de vous offrir une expérience unique et revitalisante. Que vous soyez à la recherche d'un moment de détente profonde, d'un soulagement du stress, de l'élimination des blocages énergétiques ou d'une reconnexion avec votre essence intérieure, j'ai les compétences et les techniques pour répondre à vos besoins spécifiques. j'utilise des produits bio et naturels.</p>
+            </div>
+
+            <div className="prestation">
+                <h2>Les prestations</h2>
+                <div className="card">
+                    <h3>Massages Bien-Être Harmonisant</h3>
+                    <div className="img_card1">
+                        <button>En savoir plus</button>
+                    </div>
+                </div>
+            </div>
+
+
+        </main>
     );
 }
