b814c3440a8ecc3dd0fe114f90a37227d85103aa
Mikadevfr <mikadevfr@gmail.com>
2024-11-03 21:02:07 +0100

    Page Contact

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,44 +1,11 @@
-import PictoPhone from "../PictoPhone";
-import PictoPinpoint from "../PictoPinpoint";
-import PictoMail from "../PictoMail";
-import PictoFB from "../PictoFB";
-import PictoInsta from "../PictoInsta";
+import ContactInfos from "../ContactInfos";
 import "./style.css";
 
 export default function Header() {
 
     return (
         <footer>
-            
-            <div className='contacts'>
-                <h2>Contacts</h2>
-
-                <div className="contacts_infos">
-                    <PictoPhone className='picto_phone_footer' fillPhoneColor='#1E1E1E' fillCallColor='1E1E1E' />
-                    <div>Afficher le téléphone</div>
-
-                    <div className='footer_adress'><PictoPinpoint strokeColor='#1E1E1E' /></div>
-                    <div>45 rue Claude Bernard<br /> 59200 Tourcoing</div>
-
-                    <div className='footer_mail'><PictoMail strokeColor='#1E1E1E' /></div>
-                    <div>horizonbienetre5@gmail.com</div>
-                </div>
-
-            </div>
-
-            <div className="contacts_infos_next">
-                <div className='footer_fb'><PictoFB strokeColor='#1E1E1E' strokeWidth='5' /></div>
-                <div className='footer_insta'><PictoInsta fillColor='transparent' strokeColor='#1E1E1E' /></div>
-            </div>
-
-            <div className="horaires">
-                <p>Horaires :</p>
-                <p>Du lundi au samedi de 10h00 à 19h00</p>
-            </div>
-            <div className="detail_prestations">
-                <p>Prestation sur RDV</p>
-                <p>En cabine/ A domicile / En entreprise </p>
-            </div>
+            <ContactInfos />
         </footer>
     );
 }
--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,33 +1,18 @@
+import ContactInfos from "../../components/ContactInfos";
+import FormContact from "../../components/FormContact";
 import "./style.css";
-import img1 from './../../assets/massage-bien-etre.jpeg'
-import img2 from './../../assets/Soin-energetique.jpg'
-import img3 from './../../assets/Formule-detente-profonde.jpeg'
 
 export default function Contacts() {
-
-    const prestation1 = 'Massages Bien-Être Harmonisant';
-    const prestation2 = 'Les soins énergétiques';
-    const prestation3 = 'La formule détente profonde';
-
     return (
-        <main>
-            
-            <div className="illustration">
-                <div>
-                    <h1>Contacts</h1>
-                </div>
-            </div>
-           
-            <div className="Container_prestations">
-                <div className="presta">
-                    <h2>Massage IVAO</h2>
-                    <div className="img_presta1"></div>
-                    <p>Massage relaxant du corps, avec pose et utilisation de pierres chaudes pour profiter d’un moment de détente et de récupération.</p>
-                </div>
+        <main className="container_contact">
+            <div className="illustration_contact">
+                <h1>Nous contacter</h1>
             </div>
 
-            
-
+            <div className="container_light">
+                <ContactInfos />
+                <FormContact />
+            </div>
         </main>
     );
 }
