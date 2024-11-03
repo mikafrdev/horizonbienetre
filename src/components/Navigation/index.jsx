de24db8adac6ee99984b4e84d4d705c24e8fc479
Mikadevfr <mikadevfr@gmail.com>
2024-11-03 07:52:20 +0100

    Navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -3,48 +3,57 @@ import { useState, useEffect } from "react";
 import dataNavigation from "../../data/navigation.json";
 import "./style.css";
 
-const VISIBLE = 1;
-const HIDDEN = 2;
-const ENTERING = 3; //L'élement est en train de s'afficher dans le DOM selon l'animation
-const LEAVING = 4; //L'élement est en train de quitter le DOM selon l'animation
 
 export default function Navigation({ displayNav }) {
+    const VISIBLE = 1;
+    const HIDDEN = 2;
+    const ENTERING = 3; //L'élement est en train de s'afficher dans le DOM selon l'animation
+    const LEAVING = 4; //L'élement est en train de quitter le DOM selon l'animation
     console.log("Navigation - displayNav : ", displayNav);
-    let className = "";
+    
+    const [state, setState] = useState(displayNav ? VISIBLE : HIDDEN);
+    const className = state === VISIBLE ? "fade" : "fade out";
+
+    console.log("HEIGHT : ", screen.height)
+    console.log("screen.availHeight : ", screen.availHeight)
 
     useEffect(() => {
-        let init = true;
+        /* className = "fade out"; */
         console.log("useEffect");
-    });
+        if (!displayNav) {
+            setState(LEAVING);
+        } else {
+            setState((s) => (s === HIDDEN ? ENTERING : VISIBLE));
+        }
+        console.log("useEffect1 : ", state);
+    }, [displayNav]);
 
-    if(displayNav) {
+    useEffect(() => {
+        if (state === LEAVING) {
+            const timer = setTimeout(() => {
+                setState(HIDDEN);
+            }, 1000);
+            return () => {
+                clearTimeout(timer);
+            };
+        } else if (state === ENTERING) {
+            document.body.offsetHeight;
+            setState(VISIBLE);
+        }
+        console.log("useEffect2 : ", state);
+    }, [state]);
+
+    /* if(displayNav) {
         className = "fade";
         console.log("className FADE");
         } else {
             console.log("className FADE OUT ");
             className = "fade out";
             return null;
-    }
-
-    /* console.log("state : ", state) */
-
-    /* useEffect(() => {
-    console.log("visible : ", visible)
-    if (visible === "init") {
-        console.log("1 : ")
-        return null
-    }else if (visible){
-        console.log("2 : ")
-        className = "fade";
-    } else {
-        console.log("3 : ")
-    className = "fade out";
-    return null;
-}
-}, [visible]); */
+    } */
 
     return (
-        <div className={`navigation ${className}`}>
+        <div className={`navigation ${className}`} style={{minHeight: screen.availHeight}}>
             <ul>
                 {dataNavigation.map((item, index) => (
                     <li key={index}>
