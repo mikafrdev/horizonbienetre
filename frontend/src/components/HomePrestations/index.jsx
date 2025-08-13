e9d6e51be7fd27b269074cde3d7e87bb4e93a931
Mikadevfr <mikadevfr@gmail.com>
2025-08-13 10:26:22 +0200

    Update video & images responsive

--- frontend/src/components/HomePrestations/index.jsx
+++ frontend/src/components/HomePrestations/index.jsx
@@ -5,7 +5,6 @@ import "./style.css";
 export default function HomePrestations() {
    return (
       <section className="home-prestations">
-
          <h2>Les prestations</h2>
 
          <div className="home-prestations__list">
@@ -21,14 +20,50 @@ export default function HomePrestations() {
                      className="home-prestations__link"
                      data-discover="true"
                   >
-                     <img
-                        className="home-prestations__image"
-                        src={`/${img}`}
-                        alt={title}
-                        loading="lazy"
-                     />
+                     <picture>
+                        <source
+                           type="image/avif"
+                           srcSet={`
+      /${img}-600.avif 600w,
+      /${img}-800.avif 800w,
+      /${img}-1024.avif 1024w,
+      /${img}-1440.avif 1440w
+    `}
+                           sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                        />
+                        <source
+                           type="image/webp"
+                           srcSet={`
+      /${img}-600.webp 600w,
+      /${img}-800.webp 800w,
+      /${img}-1024.webp 1024w,
+      /${img}-1440.webp 1440w
+    `}
+                           sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                        />
+                        <source
+                           type="image/jpeg"
+                           srcSet={`
+      /${img}-600.jpg 600w,
+      /${img}-800.jpg 800w,
+      /${img}-1024.jpg 1024w,
+      /${img}-1440.jpg 1440w
+    `}
+                           sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                        />
+                        <img
+                           src={`/${img}-800.jpg`} // fallback si <picture> ne fonctionne pas
+                           alt={title}
+                           loading="lazy"
+                           className="home-prestations__image"
+                        />
+                     </picture>
+
                      <figcaption className="home-prestations__caption">
-                        <h3 id={`card-title-${id}`} className="home-prestations__title">
+                        <h3
+                           id={`card-title-${id}`}
+                           className="home-prestations__title"
+                        >
                            {title}
                         </h3>
                      </figcaption>
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,16 +1,33 @@
+import { useEffect, useState } from "react";
 import HomePrestations from "../../components/HomePrestations";
 import Opinions from "../../components/Opinions";
-import VideoHP from "../../assets/hp.mp4";
 import "./style.css";
 
 export default function Home() {
+   const [videoSrc, setVideoSrc] = useState("");
+
+   useEffect(() => {
+      const width = window.innerWidth;
+
+      /* console.log("Current window width:", width); */
+
+      if (width <= 800) {
+         setVideoSrc("/videos/presentation-800.mp4");
+      } else if (width <= 1024) {
+         setVideoSrc("/videos/presentation-1024.mp4");
+      } else {
+         setVideoSrc("/videos/presentation-1440.mp4");
+      }
+   }, []);
    return (
       <main className="main-content">
          <section className="section-video">
-            <video playsInline autoPlay loop muted preload="metadata">
-               <source src={VideoHP} type="video/mp4" />
-               Your browser does not support the video tag.
-            </video>
+            {videoSrc && (
+               <video playsInline autoPlay loop muted preload="metadata">
+                  <source src={videoSrc} type="video/mp4" />
+                  Your browser does not support the video tag.
+               </video>
+            )}
          </section>
 
          <HomePrestations />
