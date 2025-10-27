3bdc4cf869675f1f902be1a0765d805e8536c407
Mikadevfr <mikadevfr@gmail.com>
2025-10-27 11:48:05 +0100

    Update Polices Clean files & CSS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import 'normalize.css';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <ThemeProvider theme={theme}>
+         <RouterProvider router={router} />
+      </ThemeProvider>
+   </React.StrictMode>
+);
---
+++ frontend/src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+   typography: {
+      fontFamily: "Verdana, sans-serif",
+      h1: { fontFamily: '"Source Serif Pro", serif' },
+      h2: { fontFamily: '"Source Serif Pro", serif' },
+      h3: { fontFamily: '"Source Serif Pro", serif' },
+   },
+   components: {
+      MuiCssBaseline: {
+         styleOverrides: `
+            h1, h2, h3 {
+               font-family: "Source Serif Pro", serif !important;
+            }
+         `,
+      },
+   },
+});
+
+export default theme;
